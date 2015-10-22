f225b2992a5d19e4250736885b2c3d810f802d50
lqzhang <lqzhang@ntu.edu.sg>
2015-10-22 22:48:16 +0800

    use fix decode parameters, simplify makefile of feat,  pass

--- egs/yesno/s5/decode.sh
+++ egs/yesno/s5/decode.sh
@@ -1,7 +1,8 @@
 decode_cmd="utils/run.pl"
 # Decoding
-
+. ./path.sh
 printf "\n*******************************Decoding *******************************\n"
-steps/decode.sh --nj 1 --cmd "$decode_cmd" \
-    exp/mono0a/graph_tgpr data/test_yesno exp/mono0a/decode_test_yesno
+
+gmm-latgen-faster --max-active=7000 --beam=13.0 --lattice-beam=6.0 --acoustic-scale=0.083333 --allow-partial=true --word-symbol-table=exp/mono0a/graph_tgpr/words.txt exp/mono0a/final.mdl exp/mono0a/graph_tgpr/HCLG.fst 'ark,s,cs:apply-cmvn  --utt2spk=ark:data/test_yesno/split1/1/utt2spk scp:data/test_yesno/split1/1/cmvn.scp scp:data/test_yesno/split1/1/feats.scp ark:- | add-deltas  ark:- ark:- |' 'ark:|gzip -c > exp/mono0a/decode_test_yesno/lat.1.gz' 
+
 printf "\n*******************************Decoding *******************************\n"
